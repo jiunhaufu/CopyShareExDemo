dd5cc834a5f150e4f2728d9c5979d022ed99f25f
傅俊皓 <alfie.fu@toplogis.com>
2017-11-10 09:47:15 +0800

    增加外部可連結的activity

---
+++ app/src/main/java/fu/alfie/com/copyshareexdemo/ExportTrueActivity.java
@@ -0,0 +1,13 @@
+package fu.alfie.com.copyshareexdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExportTrueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_export_true);
+    }
+}
